00fe25fb3e0f6f06e47b1d4a90117361a0d442d1
josh benza <joshua.benza@student.tamdistrict.org>
2025-06-10 10:59:21 -0700

    6-5

--- Chapter 1/Unit 1/Project 6/NumbersRiddle.java
+++ Chapter 1/Unit 1/Project 6/NumbersRiddle.java
@@ -3,8 +3,8 @@ public class NumbersRiddle
     public static void main(String[] args)
     {
         // variables for testing
-        int positive = 7;
-        int negative = -7;
+        int positiveInt = 7;
+        int negativeInt = -7;
         int zero = 0;
         int one = 1;
         double positiveDouble = 1.243;
@@ -12,23 +12,25 @@ public class NumbersRiddle
 
         /*
          * assign number to start with
-         * if it's an int add (double)
+         * use any int or double or use testing variables
          */
-        double startNumber = negativeDouble;
+        double startNumber = positiveInt;
 
         // calculations
         double doubleIt = startNumber * 2;
         double addSix = doubleIt + 6;
         double divideInHalf = addSix / 2;
         double subtractStart = divideInHalf - startNumber;
-        double finalResult = subtractStart;
+        int finalResult = (int) subtractStart;
 
-        // print output
+        // print numbers and calculations
         System.out.println("Starting number: " + startNumber);
+        System.out.print("\n");
         System.out.println("Doubled: " + startNumber + " * 2 = " + doubleIt);
         System.out.println("Add 6: " + doubleIt + " + 6 = " + addSix);
         System.out.println("Divide in half: " + addSix + " / 2 = " + divideInHalf);
         System.out.println("Subtract starting number: " + divideInHalf + " - " + startNumber + " = " + subtractStart);
+        System.out.print("\n");
         System.out.println("Final result: " + finalResult);
     }
 }
